57d83895f95d7d7b152922ae66da2276fd7613c4
Fang <windmaomao@gmail.com>
2020-02-19 15:41:25 -0500

    add logo animation

--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import img from './logo.png'
+
+const spin = keyframes`
+  from { transform: rotate(0deg) }
+  to { transform: rotate(360deg) }
+`
 
 const LogoDiv = styled.img`
   width: 24px;
   margin-right: 5px;
+  animation: ${spin} infinite 20s linear
 `
 
 const Logo = () => (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Book } from 'react-brandbook'
 import Logo from './Logo'
 import blog from './blog'
 
-const fontFamily = "Alegreya, serif, '宋体'"
+const fontFamily = "Average, serif, '宋体'"
 const themes = [
   { title: 'Light', theme: { mode: 'light', fontFamily } },
   { title: 'Dark', theme: { mode: 'dark', fontFamily } }
